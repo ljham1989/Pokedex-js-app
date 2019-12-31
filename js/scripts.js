08f6a32e7a6ab6c0e23303e520267a9905d42cf9
LJ Hamilton <57067351+ljham1989@users.noreply.github.com>
2019-12-31 11:50:06 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 var repository = [
   {name: "Bulbasaur", height: 0.7, type: ["grass", "poison"]},
   {name: "Ivysaur", height: 1, type: ["grass", "poison"]},
-  {name: "Venusaur", height: 1, type: ["grass", "poison"]}
+  {name: "Venusaur", height: 2, type: ["grass", "poison"]}
 ];
 
 for (var i = 0; i < repository.length; i++) {
